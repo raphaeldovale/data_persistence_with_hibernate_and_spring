7af73116c12b5cf851169a153026131f6e4d5e1a
Raphael do Vale A. Gomes <raphael@dovale.net>
2018-10-29 00:46:40 -0300

    Raw Application

---
+++ raw-hibernate/src/main/java/net/dovale/Application.java
@@ -0,0 +1,21 @@
+package net.dovale;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class Application {
+
+    public static void main(String[] args){
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure()
+                .build();
+
+        SessionFactory sessionFactory = new MetadataSources(registry)
+                .buildMetadata()
+                .buildSessionFactory();
+
+        sessionFactory.openSession();
+    }
+}
